a56623e1703ac244f45f7e9af7f6f4586de41f64
Shelley Wong <swong26@mail.csuchico.edu>
2016-10-21 09:47:24 -0700

    added style features and preliminary js code

--- intelligence_test.js
+++ intelligence_test.js
@@ -1,6 +1,15 @@
 // Author: Shelley Wong
 function question1(){
-
+  var answerSubmitted = document.getElementById("userAnswer").value;
+  var correctAnswer = 130;
+  if(correctAnswer == answerSubmitted)
+  {
+    document.getElementById("incorrect").innerHTML = "Correct!";
+  }
+  else
+  {
+    document.getElementById("incorrect").innerHTML = "Incorrect. Try again!";
+  }
 }
 
 function question2(){
@@ -8,5 +17,5 @@ function question2(){
 }
 
 function question3(){
-  
+
 }
