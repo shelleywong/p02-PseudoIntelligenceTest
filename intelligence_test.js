f340e403821ac1e0ca9ee4b142790997b2c9cd88
Shelley Wong <swong26@mail.csuchico.edu>
2016-10-21 21:25:55 -0700

    fixed some project requirements for question 1

--- intelligence_test.js
+++ intelligence_test.js
@@ -2,13 +2,19 @@
 function question1(){
   var answerSubmitted = document.getElementById("userAnswer").value;
   var correctAnswer = 130;
+  //check if submission is NOT a number; if NaN,tell user to try again
+  var notANum = isNaN(answerSubmitted);
   if(correctAnswer == answerSubmitted)
   {
-    document.getElementById("incorrect").innerHTML = "Correct!";
+    document.getElementById("output").innerHTML = "Correct!";
+  }
+  else if(notANum)
+  {
+    document.getElementById("output").innerHTML = "The answer must be a number; please try again!"
   }
   else
   {
-    document.getElementById("incorrect").innerHTML = "Incorrect. Try again!";
+    document.getElementById("output").innerHTML = "Incorrect; please try again!";
   }
 }
 
@@ -18,4 +24,8 @@ function question2(){
 
 function question3(){
 
+}
+
+function newPage(){
+  window.location.href="question2.html";
 }
