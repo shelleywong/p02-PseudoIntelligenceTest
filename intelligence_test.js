b292432cceca52996471db9cbe12da660d6d89c9
Shelley Wong <swong26@mail.csuchico.edu>
2016-10-26 17:51:43 -0700

    added js code for Q2 and Q3

--- intelligence_test.js
+++ intelligence_test.js
@@ -2,7 +2,7 @@
 function question1(){
   var answerSubmitted = document.getElementById("userAnswer").value;
   var correctAnswer = 130;
-  //check if submission is NOT a number; if NaN,tell user to try again
+  //check that submission is a number; if NaN,tell user to try again
   var notANum = isNaN(answerSubmitted);
   if(correctAnswer == answerSubmitted)
   {
@@ -10,20 +10,56 @@ function question1(){
   }
   else if(notANum)
   {
-    document.getElementById("output").innerHTML = "The answer must be a number; please try again!"
+    document.getElementById("output").innerHTML = "The answer must be a number;"
+      + " please try again!"
   }
   else
   {
-    document.getElementById("output").innerHTML = "Incorrect; please try again!";
+    document.getElementById("output").innerHTML = "The answer is a different"
+      + " number; please try again!";
   }
 }
 
 function question2(){
-
+  var answerSubmitted = document.getElementById("userAnswer").value;
+  var correctAnswer = "word";
+  //check if submission is not a number; if it is a number, user must try again
+  var notANum = isNaN(answerSubmitted);
+  if(correctAnswer == answerSubmitted)
+  {
+    document.getElementById("output").innerHTML = "Correct!";
+  }
+  else if(!notANum)
+  {
+    document.getElementById("output").innerHTML = "The answer must be a string"
+    + " of alphabetic characters; please try again!";
+  }
+  else
+  {
+    document.getElementById("output").innerHTML = "The answer is a different"
+    + " string of characters; please try again!";
+  }
 }
 
 function question3(){
-
+  var answerSubmitted = document.getElementById("userAnswer").value;
+  var correctAnswer = 42;
+  //check that submission is a number; if NaN, tell user to try again
+  var notANum = isNaN(answerSubmitted);
+  if(correctAnswer == answerSubmitted)
+  {
+    document.getElementById("output").innerHTML = "Correct!";
+  }
+  else if(notANum)
+  {
+    document.getElementById("output").innerHTML = "The answer must be a number;"
+      + " please try again!"
+  }
+  else
+  {
+    document.getElementById("output").innerHTML = "The answer is a different"
+      + " number; please try again!";
+  }
 }
 
 function newPage(){
